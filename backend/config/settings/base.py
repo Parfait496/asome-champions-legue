cca6cc939f8480389e01f79393341eae17a481f1
Parfait <parfaitndizihiwe@gmail.com>
2026-06-09 20:52:24 +0200

    Fix CORS allow all origins config

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -124,6 +124,8 @@ CORS_ALLOWED_ORIGINS = config(
 ).split(',')
 CORS_ALLOW_CREDENTIALS = True
 
+CORS_ALLOW_ALL_ORIGINS = config('CORS_ALLOW_ALL_ORIGINS', default=False, cast=bool)
+
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'Africa/Kigali'
 USE_I18N = True
